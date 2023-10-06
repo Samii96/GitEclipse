4ef1d909c6fc639d31c5036be04353a9cad8dfce
Samii96 <sami.raty@student.hamk.fi>
2023-10-06 17:26:23 +0300

    Muuttujien ja scannerin määrittäminen

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner in = new Scanner (System.in);
+		
+		String name = "Sami";
+		String guess;
+		int attempts = 0;
+		boolean correctGuess = false;
+		boolean userWantsToQuit = false;
+		
 	}
-
+		
 }
+			
+			
